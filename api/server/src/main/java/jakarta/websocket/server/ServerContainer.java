a2108581d5b501c8387b7426568a0ea227263586
Mark Thomas <markt@apache.org>
2021-10-07 13:27:38 +0100

    Relax restriction on timing of deployment. Clarify language.

--- api/server/src/main/java/jakarta/websocket/server/ServerContainer.java
+++ api/server/src/main/java/jakarta/websocket/server/ServerContainer.java
@@ -25,12 +25,13 @@ import jakarta.websocket.*;
 /**
  * The ServerContainer is the specialized view of the WebSocketContainer available in server-side deployments. There is
  * one ServerContainer instance per websocket application. The ServerContainer holds the methods to be able to register
- * server endpoints during the initialization phase of the application.
+ * server endpoints.
  * <p>
  * For websocket enabled web containers, developers may obtain a reference to the ServerContainer instance by retrieving
  * it as an attribute named <code>jakarta.websocket.server.ServerContainer</code> on the ServletContext. This way, the
  * registration methods held on this interface may be called to register server endpoints from a ServletContextListener
- * during the deployment of the WAR file containing the endpoint.
+ * during the deployment of the WAR file containing the endpoint. Server endpoints may also be registered after the web
+ * application has started.
  * </p>
  * <p>
  * WebSocket implementations that run outside the web container may have other means by which to provide a
@@ -45,8 +46,7 @@ import jakarta.websocket.*;
 public interface ServerContainer extends WebSocketContainer {
 
     /**
-     * Deploys the given annotated endpoint into this ServerContainer during the initialization phase of deploying the
-     * application.
+     * Deploys the given annotated endpoint into this ServerContainer.
      *
      * @param endpointClass the class of the annotated endpoint
      * @throws DeploymentException   if the annotated endpoint was badly formed.
@@ -55,6 +55,7 @@ public interface ServerContainer extends WebSocketContainer {
     public void addEndpoint(Class<?> endpointClass) throws DeploymentException;
 
     /**
+     * Deploys the given endpoint described by the provided configuration into this ServerContainer.
      *
      * @param serverConfig the configuration instance representing the logical endpoint that will be registered.
      * @throws DeploymentException   if the endpoint was badly formed.
@@ -66,7 +67,8 @@ public interface ServerContainer extends WebSocketContainer {
      * Upgrade the HTTP connection represented by the {@code HttpServletRequest} and {@code HttpServletResponse} to the
      * WebSocket protocol and establish a WebSocket connection as per the provided {@link ServerEndpointConfig}.
      * <p>
-     * This method is primarily intended to be used by frameworks that implement the front-controller pattern.
+     * This method is primarily intended to be used by frameworks that implement the front-controller pattern. It does
+     * not deploy the provided endpoint.
      * <p>
      * If the WebSocket implementation is not deployed as part of a Jakarta Servlet container, this method will throw an
      * {@link UnsupportedOperationException}.
